de636a0cde699aa2d4e54be4d822971b322adb98
jinrup <tom875694231@gmail.com>
2026-01-24 22:59:56 +0800

    update

--- .config/nvim/lua/tom/plugins/harpoon.lua
+++ .config/nvim/lua/tom/plugins/harpoon.lua
@@ -18,34 +18,6 @@ return {
 			end,
 			desc = "Harpoon menu",
 		},
-		{
-			"<C-h>",
-			function()
-				require("harpoon"):list():select(1)
-			end,
-			desc = "Harpoon file 1",
-		},
-		{
-			"<C-j>",
-			function()
-				require("harpoon"):list():select(2)
-			end,
-			desc = "Harpoon file 2",
-		},
-		{
-			"<C-k>",
-			function()
-				require("harpoon"):list():select(3)
-			end,
-			desc = "Harpoon file 3",
-		},
-		{
-			"<C-l>",
-			function()
-				require("harpoon"):list():select(4)
-			end,
-			desc = "Harpoon file 4",
-		},
 	},
 	opts = {},
 }

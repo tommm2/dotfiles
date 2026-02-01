9f91d2dd9a175c85a6f0a2784f4492b21a888117
jinrup <tom875694231@gmail.com>
2026-02-01 20:39:57 +0800

    update plugin

--- nvim/.config/nvim/lua/tom/plugins/cmp.lua
+++ nvim/.config/nvim/lua/tom/plugins/cmp.lua
@@ -11,24 +11,21 @@ return {
 	{
 		"saghen/blink.cmp",
 		dependencies = {
-			"onsails/lspkind.nvim", -- 加這個即可
+			"onsails/lspkind.nvim",
 		},
 		event = { "BufReadPre", "BufNewFile" },
 		version = "*",
 		opts = function()
 			local lspkind = require("lspkind")
-			lspkind.init({ preset = "codicons" }) -- 啟用 VSCode icon
+			lspkind.init({ preset = "codicons" })
 
 			return {
 				keymap = {
 					preset = "default",
 					["<C-j>"] = { "select_next", "fallback" },
 					["<C-k>"] = { "select_prev", "fallback" },
-					["<CR>"] = { "select_and_accept", "fallback" },
-					["<C-u>"] = { "scroll_documentation_up", "fallback" },
-					["<C-d>"] = { "scroll_documentation_down", "fallback" },
 				},
-				appearance = { nerd_font_variant = "mono" },
+				appearance = { use_nvim_cmp_as_default = true, nerd_font_variant = "mono" },
 				completion = {
 					documentation = { auto_show = true, window = { border = "rounded" } },
 					list = { selection = { preselect = true } },
